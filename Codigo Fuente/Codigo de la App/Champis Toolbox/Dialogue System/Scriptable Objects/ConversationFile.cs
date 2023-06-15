7b7a8f95874e6ffabd4251c2d824ff984e6926cf
IDGG <tlozmm64@gmail.com>
2023-06-15 09:49:47 -0600

    Reset

--- Codigo Fuente/Codigo de la App/Champis Toolbox/Dialogue System/Scriptable Objects/ConversationFile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Conversation File", menuName = "Champis/Dialogue/Conversation File")]
-public class ConversationFile : ScriptableObject
-{
-    public Dialogue[] dialogues;
-}
