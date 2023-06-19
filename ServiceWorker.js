7a5dba99e58a95c5b627fe31f835b746327036b5
IDGG <tlozmm64@gmail.com>
2023-06-19 01:16:44 -0600

    PWA Test

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Nancy Ximena Gaytán Guerra-Calculadora-1.0.0";
+const contentToCache = [
+    "Build/Calc PWA.loader.js",
+    "Build/Calc PWA.framework.js",
+    "Build/Calc PWA.data",
+    "Build/Calc PWA.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
